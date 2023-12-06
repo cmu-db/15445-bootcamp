80b9d06b1759404ce9005a3b7a1e931086dbe3b9
Jixin Liang <110170603+cscourage@users.noreply.github.com>
2023-12-06 10:47:41 +0800

    Update move_constructors.cpp, add a copy assignment demo which causes compiler when you uncomment it.

--- src/move_constructors.cpp
+++ src/move_constructors.cpp
@@ -133,10 +133,13 @@ int main() {
   andy1.PrintValid();
 
   // However, note that because the copy assignment operator and copy
-  // constructor are deleted, this code will not compile. The first line uses
-  // the copy constructor and the second line also uses the copy constructor.
-  // Try uncommenting one line at a time to see the resulting compiler errors.
-  // Person andy3 = andy2;
+  // constructor are deleted, this code will not compile. The first two lines 
+  // use the default constructor and then copy assignment operator and the last line 
+  // uses the copy constructor. Try uncommenting one part at a time to see the 
+  // resulting compiler errors.
+  // Person andy3;
+  // andy3 = andy2;
+  
   // Person andy4(andy2);
 
   return 0;
